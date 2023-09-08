2c026d52e91e7a86d379d0fc4b2ef9513e875a4b
Shashank <shashanke7y@gmail.com>
2023-09-08 19:59:33 -0400

    link navlinks to their designated pages

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,18 +1,25 @@
+import Link from "next/link";
 import React from "react";
 
 const HeaderComponent = () => {
   return (
     <div className="h-10 flex items-center justify-evenly mt-5">
-      <div className="">
+      <div>
         <h1 className="text-lg font-bold">shashank ellareddy</h1>
       </div>
-      <div className="">home 🏠</div>
-      <div className="text-gray-700 hover:text-white hover:underline">
-        github 💻
-      </div>
-      <div className="text-gray-700 hover:text-white hover:underline">
-        blog ✍️
-      </div>
+      <Link href="/">
+        <div>home 🏠</div>
+      </Link>
+      <Link href="/github">
+        <div className="text-gray-700 hover:text-white hover:underline">
+          github 💻
+        </div>
+      </Link>
+      <Link href="blog">
+        <div className="text-gray-700 hover:text-white hover:underline">
+          blog ✍️
+        </div>
+      </Link>
     </div>
   );
 };
